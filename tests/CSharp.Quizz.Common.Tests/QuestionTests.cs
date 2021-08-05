d18cbb736e7bffc22b14665823bcb3000f7d17ad
Raphael Camine <raphael.camine@gestaosat.com>
2021-08-05 01:30:18 -0300

    Changing folder structure and adding common tests

---
+++ tests/CSharp.Quizz.Common.Tests/QuestionTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CSharp.Quizz.Common.Tests
+{
+    public class QuestionTests
+    {
+        [Fact(DisplayName = "When a new question is initialized should add a new valid Guid")]
+        public void When_NewQuestion_Should_AddValidNewGuid()
+        {
+            var question = new Question("Test Question", Difficulty.Easy, Category.ComputerScience);
+            question.Id.Should().NotBeEmpty();
+        }
+    }
+}
